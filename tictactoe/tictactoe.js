26e7e5f683f6d8f8cd07c054b320e55a8e61f845
Nate Armstrong <nate@custombit.com>
2013-09-01 19:26:22 -0600

    Change tictactoe to use constructor notation.

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,10 +1,3 @@
-var playerOneTurn = true;
-var _x = 'x'
-var _o = 'o'
-var gridSize = 3;
-var tdElements = Array(Array(3), Array(3), Array(3));
-var winnerCells;
-var winner;
 
 String.prototype.getY = function() {
   return Number(this.slice(-1));
@@ -14,65 +7,82 @@ String.prototype.getX = function() {
   return Number(this.slice(-2)[0]);
 }
 
-function getPlayer() {
-  return playerOneTurn ? _x : _o
-}
+var tictactoe = function() {
 
-function playerIsAtCell(x, y) {
-  if (x < 0 || x > gridSize -1 || y < 0 || y > gridSize -1) return false;
-  else {
-    return tdElements[x][y].innerHTML === getPlayer();
-  }
-}
-
-function checkNeighbors(x, y, j, k) {
-  if (playerIsAtCell(x, y) && playerIsAtCell(j, k)) {
-    winner = true;
-    winnerCells.push(tdElements[x][y], tdElements[j][k]);
-  }
-}
-
-function checkForWinner(x, y) {
-  winnerCells = Array(tdElements[x][y]);
-  winner = false;
-  checkNeighbors(x+1, y, x+2, y)      // right
-  checkNeighbors(x+1, y+1, x+2, y+2)  // right diag down
-  checkNeighbors(x, y+1, x, y+2)      // down
-  checkNeighbors(x-1, y+1, x-2, y+2)  // left diag down
-  checkNeighbors(x-1, y, x-2, y)      // left
-  checkNeighbors(x-1, y-1, x-2, y-2)  // left diag up
-  checkNeighbors(x, y-1, x, y-2)      // up
-  checkNeighbors(x+1, y-1, x+2, y-2)  // right diag up
-  checkNeighbors(x-1, y, x+1, y)      // left and right
-  checkNeighbors(x, y-1, x, y+1)      // up and down
-  checkNeighbors(x+1, y-1, x-1, y+1)  // right diag up and left diag down
-  checkNeighbors(x-1, y-1, x+1, y+1)  // left diag up and right diag down
-
-  if (winner && winnerCells.length > 2) {
-    var i;
-    for (i=0; i<winnerCells.length; i++) {
-      winnerCells[i].className = 'winner';
+  var _this = this;
+  this.initialize = function() {
+    _this.playerOneTurn = true,
+    _this._x = 'x',
+    _this._o = 'o',
+    _this.gridSize = 3,
+    _this.tdElements = Array(Array(3), Array(3), Array(3)),
+    _this.winnerCells = Array(),
+    _this.winner = false;
+    var cells, i;
+    cells = document.getElementsByTagName('td');
+    for (i=0; i<cells.length; i++) {
+      cells[i].onclick = _this.handleCellClick;
+      _this.tdElements[i%_this.gridSize][Math.floor(i/_this.gridSize)] = cells[i];
     }
-    alert(getPlayer() + ' won!');
-    document.location.reload();
-  }
-  playerOneTurn = !playerOneTurn;
+  };
+
+  this.getPlayer = function() {
+    return this.playerOneTurn ? _this._x : _this._o;
+  };
+
+  this.playerIsAtCell = function(x, y) {
+    if (x < 0 || x > this.gridSize -1 || y < 0 || y > this.gridSize -1) return false;
+    else {
+      return this.tdElements[x][y].innerHTML === this.getPlayer();
+    }
+  };
+
+  this.checkNeighbors = function(x, y, j, k) {
+    if (this.playerIsAtCell(x, y) && this.playerIsAtCell(j, k)) {
+      this.winner = true;
+      this.winnerCells.push(this.tdElements[x][y], this.tdElements[j][k]);
+    }
+  };
+
+  this.checkForWinner = function(x, y) {
+    var _this = this;
+    _this.winnerCells = Array(_this.tdElements[x][y]);
+    _this.winner = false;
+    _this.checkNeighbors(x+1, y, x+2, y)      // right
+    _this.checkNeighbors(x+1, y+1, x+2, y+2)  // right diag down
+    _this.checkNeighbors(x, y+1, x, y+2)      // down
+    _this.checkNeighbors(x-1, y+1, x-2, y+2)  // left diag down
+    _this.checkNeighbors(x-1, y, x-2, y)      // left
+    _this.checkNeighbors(x-1, y-1, x-2, y-2)  // left diag up
+    _this.checkNeighbors(x, y-1, x, y-2)      // up
+    _this.checkNeighbors(x+1, y-1, x+2, y-2)  // right diag up
+    _this.checkNeighbors(x-1, y, x+1, y)      // left and right
+    _this.checkNeighbors(x, y-1, x, y+1)      // up and down
+    _this.checkNeighbors(x+1, y-1, x-1, y+1)  // right diag up and left diag down
+    _this.checkNeighbors(x-1, y-1, x+1, y+1)  // left diag up and right diag down
+
+    if (_this.winner && _this.winnerCells.length > 2) {
+      var i;
+      for (i=0; i<_this.winnerCells.length; i++) {
+        _this.winnerCells[i].className = 'winner';
+      }
+      alert(_this.getPlayer() + ' won!');
+      document.location.reload();
+    }
+    _this.playerOneTurn = !_this.playerOneTurn;
+  };
+
+  this.handleCellClick = function() {
+    if (this.innerHTML === _this._x || this.innerHTML === _this._o) return;
+    else {
+      var x, y;
+      x = this.id.getX();
+      y = this.id.getY();
+      this.innerHTML = _this.getPlayer();
+      _this.checkForWinner(x, y);
+    }
+  };
 }
 
-function handleCellClick() {
-  if (this.innerHTML === _x || this.innerHTML === _o) return;
-  else {
-    var x, y;
-    x = this.id.getX();
-    y = this.id.getY();
-    this.innerHTML = getPlayer();
-    checkForWinner(x, y);
-  }
-}
-
-var cells, i;
-cells = document.getElementsByTagName('td');
-for (i=0; i<cells.length; i++) {
-  cells[i].onclick = handleCellClick;
-  tdElements[i%gridSize][Math.floor(i/gridSize)] = cells[i];
-}
+var ttt = new tictactoe();
+ttt.initialize();
