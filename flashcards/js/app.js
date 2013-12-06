07869a3aec65dca19b86826285d3af87643f203e
Nate Armstrong <nate@custombit.com>
2013-12-05 21:53:03 -0700

    Flashcards get persisted and study mostly works.

--- flashcards/js/app.js
+++ flashcards/js/app.js
@@ -1,10 +1,7 @@
-var flashcards = angular.module('flashcards', ['ngRoute']);
+var flashcards = angular.module('flashcards', ['ngRoute', 'ngResource']);
 
-flashcards.config([
-  "$httpProvider", function($httpProvider) {
-    // $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-  }
-])
+flashcards.config(['$httpProvider','$locationProvider', function ($httpProvider, $locationProvider) {
+ }])
 .config(function($routeProvider, $locationProvider) {
   $routeProvider.when('/', {
     templateUrl: 'welcome.html',
@@ -16,5 +13,15 @@ flashcards.config([
   .when('/decks/:deckId', {
     templateUrl: 'decks/index.html',
     controller: 'DecksCtrl as deckCtrl'
-  });
+  })
+  .when('/decks/:deckId/study', {
+    templateUrl: 'decks/study.html',
+    controller: 'StudyCtrl as studyCtrl'
+  })
 });
+
+flashcards.factory('Deck', ['$resource', function($resource) {
+  return $resource('http://flashcardsapi.dev/decks/:deckId', {deckId: '@id'}, {
+    put: {method: 'PUT', params: { '_method': 'PUT'}}
+  });
+}])
--- flashcards/js/controllers.js
+++ flashcards/js/controllers.js
@@ -1,27 +1,48 @@
-flashcards.controller('DecksCtrl', ['$scope', '$http', '$filter', '$timeout', '$routeParams', function DecksCtrl($scope, $http, $filter, $timeout, $routeParams) {
-  $scope.currentDeck = {}
-  $scope.decks = [
-    {
-      title: 'IT Acronyms',
-      cards: [
-        {front: 'OSI', back: 'Open Systems Interconnection'},
-        {front: 'STP', back: 'Shielded Twisted Pair'}
-      ]
-    }
-  ]
+flashcards.controller('DecksCtrl', ['$scope', '$http', '$filter', '$timeout', '$routeParams', '$location', 'Deck', function DecksCtrl($scope, $http, $filter, $timeout, $routeParams, $location, Deck) {
 
-  this.resetDeck = function() {
-    $scope.currentDeck.deck = {
-      title: 'New Deck',
+  // var newDeck = new Deck({name: 'New Deck'});
+  // newDeck.$save();
+  // console.log(newDeck);
+
+  $scope.currentDeck = {}
+
+  this.resetDecks = function() {
+    Deck.query(function(decks) {
+      $scope.decks = decks;
+    });
+    $scope.currentDeck.deck = new Deck({
+      name: 'New Deck',
       cards: []
-    }
+    })
     addBlankCardsToDeck();
   }
-  this.resetDeck();
+  this.resetDecks();
 
   this.selectDeck = function(deck) {
     $scope.currentDeck.deck = angular.copy(deck);
+    console.log(deck);
     addBlankCardsToDeck();
+  }
+
+  this.studyDeck = function(deck) {
+    deck.cards = removeBlankCardsFromDeck(deck);
+    window.studyDeck = deck;
+    $location.path('/decks/' + 'undefined' + '/study')
+  }
+
+  this.saveDeck = function() {
+    var _this = this;
+    var deck = $scope.currentDeck.deck;
+    deck.cards_attributes = removeBlankCardsFromDeck(deck);
+    if (deck.id) {
+      deck.$put(function() {
+        _this.resetDecks();
+      });
+    } else {
+      deck.$save(function() {
+        _this.resetDecks();
+      });
+    }
   }
 
   // private
@@ -36,7 +57,55 @@ flashcards.controller('DecksCtrl', ['$scope', '$http', '$filter', '$timeout', '$
     }
   }
 
+  function removeBlankCardsFromDeck(deck) {
+    var cards = deck.cards;
+    var newCards = [];
+    for (var i=0; i<cards.length; i++) {
+      if (cards[i].front && cards[i].back && cards[i].front.length > 0 && cards[i].back.length > 0) {
+        newCards.push(cards[i])
+      }
+    }
+    return newCards;
+  }
+
 }]);
 
 flashcards.controller('WelcomeCtrl', ['$scope', '$http', '$filter', '$timeout', function WelcomeCtrl($scope, $http, $filter, $timeout) {
+}]);
+
+flashcards.controller('StudyCtrl', ['$scope', '$http', '$filter', '$timeout', '$routeParams', 'Deck', function StudyCtrl($scope, $http, $filter, $timeout, $routeParams, Deck) {
+  if ($routeParams.deckId === 'undefined') {
+    if (window.studyDeck) {
+      $scope.deck = window.studyDeck;
+    } else {
+      // we didn't find a deck. redirect or so show a message or something
+    }
+  } else {
+    $scope.deck = Deck.get({deckId: $routeParams.deckId});
+    console.log($scope.deck);
+  }
+  $scope.currentCardIndex = 0;
+  $scope.currentSide = 'front';
+
+  this.flipCard = function() {
+    $scope.currentSide = $scope.currentSide == 'front' ? 'back' : 'front';
+  }
+
+  this.nextCard = function() {
+    if ($scope.currentCardIndex >= $scope.deck.cards.length - 1) {
+      $scope.currentCardIndex = 0;
+    } else {
+      $scope.currentCardIndex += 1;
+    }
+    $scope.currentSide = 'front';
+  }
+
+  this.prevCard = function() {
+    if ($scope.currentCardIndex <= 0) {
+      $scope.currentCardIndex = $scope.deck.cards.length - 1;
+    } else {
+      $scope.currentCardIndex -= 1;
+    }
+    $scope.currentSide = 'front';
+  }
 }]);
