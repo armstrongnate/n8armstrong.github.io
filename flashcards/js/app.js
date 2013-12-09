f060ae4d2492a67a385a57c49b179a78ae40c38e
Nate Armstrong <nate@custombit.com>
2013-12-08 17:08:18 -0700

    Updates mobile version, adds spinner, adds google drive test. - you can now swipe flashcards from left to right or tap to flip - spinner is displayed when loading decks - hopefully CORS error with google drive request is just a localhost thing.

--- flashcards/js/app.js
+++ flashcards/js/app.js
@@ -1,4 +1,4 @@
-var flashcards = angular.module('flashcards', ['ngRoute', 'ngResource']);
+var flashcards = angular.module('flashcards', ['ngRoute', 'ngResource', 'ngTouch']);
 
 flashcards.config(['$httpProvider','$locationProvider', function ($httpProvider, $locationProvider) {
  }])
@@ -10,9 +10,13 @@ flashcards.config(['$httpProvider','$locationProvider', function ($httpProvider,
     templateUrl: 'decks/index.html',
     controller: 'DecksCtrl as deckCtrl'
   })
-  .when('/decks/:deckId', {
+  .when('/decks/:deckId/edit', {
     templateUrl: 'decks/index.html',
     controller: 'DecksCtrl as deckCtrl'
+  })
+  .when('/decks/study', {
+    templateUrl: 'decks/study.html',
+    controller: 'StudyCtrl as studyCtrl'
   })
   .when('/decks/:deckId/study', {
     templateUrl: 'decks/study.html',
@@ -21,7 +25,32 @@ flashcards.config(['$httpProvider','$locationProvider', function ($httpProvider,
 });
 
 flashcards.factory('Deck', ['$resource', function($resource) {
-  return $resource('http://flashcardsapi.dev/decks/:deckId', {deckId: '@id'}, {
+  return $resource('http://flashcardsapi.192.168.0.2.xip.io/decks/:deckId', {deckId: '@id'}, {
     put: {method: 'PUT', params: { '_method': 'PUT'}}
   });
 }])
+.directive('usSpinner', ['$window', function ($window) {
+  return {
+    scope: true,
+    link: function (scope, element, attr) {
+      scope.spinner = null;
+
+      function stopSpinner() {
+        if (scope.spinner) {
+          scope.spinner.stop();
+          scope.spinner = null;
+        }
+      }
+
+      scope.$watch(attr.usSpinner, function (options) {
+        stopSpinner();
+        scope.spinner = new $window.Spinner(options);
+        scope.spinner.spin(element[0]);
+      }, true);
+
+      scope.$on('$destroy', function () {
+        stopSpinner();
+      });
+    }
+  };
+}]);
--- flashcards/js/controllers.js
+++ flashcards/js/controllers.js
@@ -1,28 +1,33 @@
 flashcards.controller('DecksCtrl', ['$scope', '$http', '$filter', '$timeout', '$routeParams', '$location', 'Deck', function DecksCtrl($scope, $http, $filter, $timeout, $routeParams, $location, Deck) {
 
-  // var newDeck = new Deck({name: 'New Deck'});
-  // newDeck.$save();
-  // console.log(newDeck);
-
+  var access_token = 'AIzaSyD-La4vdDIC6bjt9KY5hHGuxQ1uMgGaSnA';
   $scope.currentDeck = {}
+  $scope.decksSpinner = {}
+
+  var url = 'https://docs.google.com/spreadsheet/ccc?key=0Apx0jIgUFkKpdFJWQ2hFclBiaEJNT0UyMUxnQURKVnc&usp=drive_web#gid=0' + '?access_token=' + access_token;
+  $http.get(url).success(function(data) {
+    console.log(data);
+  });
 
   this.resetDecks = function() {
+    $scope.decksSpinner.spinning = true;
     Deck.query(function(decks) {
       $scope.decks = decks;
+      $scope.decksSpinner.spinning = false;
     });
-    $scope.currentDeck.deck = new Deck({
-      name: 'New Deck',
-      cards: []
-    })
-    addBlankCardsToDeck();
+    if ($routeParams.deckId) {
+      $scope.currentDeck.deck = Deck.get({deckId: $routeParams.deckId}, function() {
+        addBlankCardsToDeck();
+      });
+    } else {
+      $scope.currentDeck.deck = new Deck({
+        name: 'New Deck',
+        cards: []
+      });
+      addBlankCardsToDeck();
+    }
   }
   this.resetDecks();
-
-  this.selectDeck = function(deck) {
-    $scope.currentDeck.deck = angular.copy(deck);
-    console.log(deck);
-    addBlankCardsToDeck();
-  }
 
   this.studyDeck = function(deck) {
     deck.cards = removeBlankCardsFromDeck(deck);
@@ -43,6 +48,26 @@ flashcards.controller('DecksCtrl', ['$scope', '$http', '$filter', '$timeout', '$
         _this.resetDecks();
       });
     }
+  }
+
+  this.destroyCardAtIndex = function(index) {
+    var deck, card;
+    var _this = this;
+    deck = $scope.currentDeck.deck;
+    deck.cards_attributes = removeBlankCardsFromDeck(deck);
+    card = deck.cards_attributes[index];
+    card['_destroy'] = '1';
+    deck.cards_attributes[index] = card;
+    deck.$put(function() {
+      _this.resetDecks();
+    });
+  }
+
+  this.destroy = function() {
+    var _this = this;
+    $scope.currentDeck.deck.$delete(function() {
+      _this.resetDecks();
+    });
   }
 
   // private
@@ -74,18 +99,16 @@ flashcards.controller('WelcomeCtrl', ['$scope', '$http', '$filter', '$timeout', 
 }]);
 
 flashcards.controller('StudyCtrl', ['$scope', '$http', '$filter', '$timeout', '$routeParams', 'Deck', function StudyCtrl($scope, $http, $filter, $timeout, $routeParams, Deck) {
-  if ($routeParams.deckId === 'undefined') {
-    if (window.studyDeck) {
-      $scope.deck = window.studyDeck;
-    } else {
-      // we didn't find a deck. redirect or so show a message or something
-    }
-  } else {
+  if ($routeParams.deckId) {
     $scope.deck = Deck.get({deckId: $routeParams.deckId});
-    console.log($scope.deck);
   }
   $scope.currentCardIndex = 0;
   $scope.currentSide = 'front';
+  $scope.decksSpinner = {spinning: true};
+  Deck.query(function(decks) {
+    $scope.decks = decks;
+    $scope.decksSpinner.spinning = false;
+  });
 
   this.flipCard = function() {
     $scope.currentSide = $scope.currentSide == 'front' ? 'back' : 'front';
@@ -107,5 +130,21 @@ flashcards.controller('StudyCtrl', ['$scope', '$http', '$filter', '$timeout', '$
       $scope.currentCardIndex -= 1;
     }
     $scope.currentSide = 'front';
+  }
+
+  var _this = this;
+  document.onkeydown = function(e) {
+    switch (e.keyCode) {
+      case 38: // up arrow
+        _this.flipCard();
+        break;
+      case 39: // right arrow
+        _this.nextCard();
+        break;
+      case 37: // left arrow
+        _this.prevCard();
+        break;
+    }
+    $scope.$apply();
   }
 }]);
